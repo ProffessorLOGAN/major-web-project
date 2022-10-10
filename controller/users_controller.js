eca8df4d8b308a6d15c716985ce86c178c65995c
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-10 23:34:39 +0530

    user sign up feature implemented successfully

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
   return res.render("user_profile", {
     title: "User Profile",
@@ -11,20 +13,37 @@ module.exports.signUp = function (req, res) {
   });
 };
 
-
-//render the sign in page 
+//render the sign in page
 module.exports.signIn = function (req, res) {
   return res.render("user_sign_in", {
     title: "Coding-Keeda | sign in",
   });
 };
 
-//get the sign up data 
-module.exports.create = function(req,res){
+//get the sign up data
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
 
-}
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
+    }
 
-//sign in and create a session for the user 
-module.exports.createSession = function(req,res){
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user while signing up");
+          return res.redirect("/users/sign-in");
+        }
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
 
-}
+//sign in and create a session for the user
+module.exports.createSession = function (req, res) {};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
@@ -9,6 +10,10 @@ app.use(expressLayouts);
 //extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static("./assets"));
 
