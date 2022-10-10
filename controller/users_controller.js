42d3cbceabec518e40b5b7084cd71aee472f0607
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-10 23:06:57 +0530

    user signin signup page created

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,5 +1,30 @@
-module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
+};
+
+//render the sign  up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Coding-Keeda | sign up",
+  });
+};
+
+
+//render the sign in page 
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Coding-Keeda | sign in",
+  });
+};
+
+//get the sign up data 
+module.exports.create = function(req,res){
+
+}
+
+//sign in and create a session for the user 
+module.exports.createSession = function(req,res){
+
 }
