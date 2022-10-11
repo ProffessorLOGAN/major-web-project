42ca8489087849b11b3a04cd6fef00fc5509537e
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-11 16:12:08 +0530

    user profile setup with session successfully

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -8,6 +8,10 @@ module.exports.profile = function (req, res) {
 
 //render the sign  up page
 module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+
   return res.render("user_sign_up", {
     title: "Coding-Keeda | Sign Up",
   });
@@ -15,6 +19,10 @@ module.exports.signUp = function (req, res) {
 
 //render the sign in page
 module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+
   return res.render("user_sign_in", {
     title: "Coding-Keeda | Sign In",
   });
