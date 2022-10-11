9549f4aa7d0334cc6a8a6d7b2313482b3a06768f
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-11 17:56:35 +0530

    session cookie stored in mongo db successfully

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
 //used for session cookie
-const session  = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo");
 //extract style and scripts from sub pages into the layout
 app.use(expressLayouts);
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.use(express.urlencoded());
 
@@ -21,22 +21,32 @@ app.use(cookieParser());
 
 app.use(express.static("./assets"));
 
-
-
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use( session({
-    name:'codingKeeda',
-    //TODO change the secret before deployment  in production mode 
-    secret: 'nickfury',
+// mongo store is used to store the session cookie in the db
+app.use(
+  session({
+    name: "codingKeeda",
+    //TODO change the secret before deployment  in production mode
+    secret: "nickfury",
     saveUnitialized: false,
     resave: false,
-    cookie:{
-        maxAge:(1000*60*100)
-    }
-}))
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new MongoStore(
+      {
+        mongoUrl: "mongodb://localhost/major_web_project",
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(er || "connect-mongodb setup ok");
+      }
+    ),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,13 +54,12 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
-
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
