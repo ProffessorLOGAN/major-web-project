63f20562e0b0203330a7807e1e96a31d8deffd09
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-12 15:05:43 +0530

    post comments save in mongoDB successfully

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const homeController = require('../controller/home_controller');
+const homeController = require("../controller/home_controller");
 
-console.log('router loaded');
+console.log("router loaded");
 
-
-router.get('/', homeController.home);
-router.use('/users', require('./users'));
+router.get("/", homeController.home);
+router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
-
 
 module.exports = router;
