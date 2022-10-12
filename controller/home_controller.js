873947f4aeda618b8846bc127699cdf89e14e3c3
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-12 15:50:43 +0530

    User post Authentication added successfully

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,8 +1,23 @@
-module.exports.home = function(req, res){
-    
-    return res.render('home', {
-        title: "Home"
+const Post = require("../models/post");
+
+module.exports.home = function (req, res) {
+  //   Post.find({}, function (err, posts) {
+  //     return res.render("home", {
+  //       title: "Coding-Keeda | Home",
+  //       posts: posts,
+  //     });
+  //   });
+
+  // populate the user of each post
+
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Coding-Keeda | Home",
+        posts: posts,
+      });
     });
-}
+};
 
 // module.exports.actionName = function(req, res){}
