cab28258a2410671c207ea77553a8049d188ad7e
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-10-14 00:58:06 +0530

    profile update feature added and async await used

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
   //   Post.find({}, function (err, posts) {
   //     return res.render("home", {
   //       title: "Coding-Keeda | Home",
@@ -11,23 +11,27 @@ module.exports.home = function (req, res) {
 
   // populate the user of each post
 
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .exec(function (err, posts) {
-      User.find({}, function (err, users) {
-        return res.render("home", {
-          title: "Coding-Keeda | Home",
-          posts: posts,
-          all_users: users,
-        });
+  try {
+    let posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
+
+    let users = await User.find({});
+
+    return res.render("home", {
+      title: "Coding-Keeda | Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
 };
 
 // module.exports.actionName = function(req, res){}
--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,13 +1,27 @@
 const User = require("../models/user");
 
-module.exports.profile = function (req, res) {
-  User.findById(req.params.id, function (err, user) {
+module.exports.profile = async function (req, res) {
+  try {
+    let user = await User.findById(req.params.id);
+
     return res.render("user_profile", {
       title: "User Profile",
       profile_user: user,
-      
     });
-  });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
+};
+
+module.exports.update = function (req, res) {
+  if (req.user.id == req.params.id) {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+      return res.redirect("back");
+    });
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
 };
 
 //render the sign  up page
@@ -33,29 +47,24 @@ module.exports.signIn = function (req, res) {
 };
 
 //get the sign up data
-module.exports.create = function (req, res) {
-  if (req.body.password != req.body.confirm_password) {
-    return res.redirect("back");
-  }
-
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (err) {
-      console.log("error in finding user in signing up");
-      return;
+module.exports.create = async function (req, res) {
+  try {
+    if (req.body.password != req.body.confirm_password) {
+      return res.redirect("back");
     }
 
+    let user = await User.findOne({ email: req.body.email });
+
     if (!user) {
-      User.create(req.body, function (err, user) {
-        if (err) {
-          console.log("error in creating user while signing up");
-          return;
-        }
-        return res.redirect("/users/sign-in");
-      });
+      let user = await User.create(req.body);
+      return res.redirect("/users/sign-in");
     } else {
       return res.redirect("back");
     }
-  });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
 };
 
 //sign in and create a session for the user
